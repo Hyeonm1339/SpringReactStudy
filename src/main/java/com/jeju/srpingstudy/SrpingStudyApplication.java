added5bf7a613b822ba9fd942c44080490237a9e
Hyeonm1339 <>
2024-05-20 20:50:38 +0900

    testGit

---
+++ src/main/java/com/jeju/srpingstudy/SrpingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jeju.srpingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeju/srpingstudy/SrpingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeju.srpingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
